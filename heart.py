f3d937b9be3e4a53da6c6230fbd9a3fba9f5e99d
kurai.sx <84697122+kurai-sx@users.noreply.github.com>
2022-03-19 12:43:47 +0530

    Update and rename heart (2).py to heart.py

---
+++ heart.py
@@ -0,0 +1,32 @@
+#first we will need to import python turtle library
+from turtle import *
+
+#import the colour you want in your project
+color("red")
+
+#to start filling colour in the model
+begin_fill()
+
+#select the size of the pen so that it will create your project model in that size only
+pensize(3)
+
+#to move left
+left(50)
+
+#to move forward with the given direction
+forward(133)
+
+#to create a circle with the given angles and values
+circle(50,200)
+
+#to move right
+right(140)
+
+#to create a circle with the given angles and values
+circle(50,200)
+
+#to move forward with the given direction
+forward(133)
+
+#give the ending so it will stop filling the model
+end_fill()
